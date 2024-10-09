1e02e20ba1981c9d7d17190e12365c8ec2a5d9ea
kenko-l <kenko.latchman@gmail.com>
2024-10-09 17:27:45 -0400

    Implemented checkWin

--- TicTacToe/src/main/Game.java
+++ TicTacToe/src/main/Game.java
@@ -18,8 +18,26 @@ public class Game {
 			  var input = scanner.nextInt();
 
 			  board[input-1] = whoseTurn;
-		}
 		
+				if (  (board[0] + board[1] + board[2] == (whoseTurn * 3)) // first row 
+						   || (board[3] + board[4] + board[5] == (whoseTurn * 3)) // second row
+						   || (board[6] + board[7] + board[8] == (whoseTurn * 3)) // third row
+						   || (board[0] + board[3] + board[6] == (whoseTurn * 3)) // first column
+						   || (board[1] + board[4] + board[7] == (whoseTurn * 3)) // second column
+						   || (board[2] + board[5] + board[8] == (whoseTurn * 3)) // third column
+						   || (board[0] + board[4] + board[8] == (whoseTurn * 3)) // first diagonal
+						   || (board[2] + board[4] + board[6] == (whoseTurn * 3)) // second diagonal
+						) 
+						{
+						  printBoard(board);
+						  System.out.println("You won!!! Congratulations!");
+						  break;
+						} else {
+						  numOfSquares++;
+						  whoseTurn = (whoseTurn == 'x') ? 'o' : 'x';
+						}
+		}
+
 	}
 	
 	private static void printBoard(char[] board) 
